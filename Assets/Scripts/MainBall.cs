68c58134f045f3077b1f28ea7a7aa1f1c74de464
usford <39438766+usford@users.noreply.github.com>
2019-04-23 23:32:01 +0300

    Initial commit

---
+++ Assets/Scripts/MainBall.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainBall : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+       
+    }
+
+    void Update()
+    {
+        
+    }
+}
